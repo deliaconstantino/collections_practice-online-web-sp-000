a8b300c3ab58fc91d8f7389719cff7a3e3942c72
Delia Constantino <dconstan92@gmail.com>
2020-10-29 21:08:43 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,5 +19,6 @@ def swap_elements(array)
   array[0], array[1], array[2] = array[0], array [2], array[1]
 end
 
-swap_elements_from_to(array, index_from, index_to)
+def swap_elements_from_to(array, index_from, index_to)
 array[1,2] = array[2, 1]
+end
