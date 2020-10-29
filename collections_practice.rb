2e52e205f4a498f5a18fdde85032ef318725ad03
Delia Constantino <dconstan92@gmail.com>
2020-10-29 21:50:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -43,13 +43,16 @@ def sum_array(array)
 end
 
 def add_s(array)
-  new_array = []
-  array.each_with_index do |word, index|
-    if index == 1
-      new_array << word
-    else
-      new_array << "#{word}s"
-    end
-  end
-  new_array
-end
+  array.each_with_index.collect
+
+# def add_s(array)
+#   new_array = []
+#   array.each_with_index do |word, index|
+#     if index == 1
+#       new_array << word
+#     else
+#       new_array << "#{word}s"
+#     end
+#   end
+#   new_array
+# end
