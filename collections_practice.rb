839650c32e517fa206b957cc0cf01b12d032da87
Delia Constantino <dconstan92@gmail.com>
2020-10-29 21:01:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -16,6 +16,8 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array[1], array[2] = array [2], array[1]
-
+  new_array = []
+  new_array.push array[0]
+  new_array.push (array[1], array[2] = array [2], array[1])
+  
 end
